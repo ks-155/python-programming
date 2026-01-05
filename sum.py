0a63f65566cf859a68b175c119aad73b97014b76
ks-155 <158799414+ks-155@users.noreply.github.com>
2026-01-05 15:44:23 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,53 @@
+#a=int(input("enter num 1:"))
+#b=int(input("enter num 2:"))
+#print(a+b)
+
+#subtract two num
+#a=int(input("enter num 1:"))
+#b=int(input("enter num 2:"))
+#print(a-b)
+
+#multiply two numbers
+#a=int(input("enter num 1:"))
+#b=int(input("enter num 2:"))
+#print(a*b)
+
+
+#multiply two numbers
+#a=int(input("enter num 1:"))
+#b=int(input("enter num 2:"))
+#print(a/b)
+
+#a=int(input("enter num 1:"))
+#b=int(input("enter num 2:"))
+#print(a+b)
+#print(a-b)
+#print(a*b)
+#print(a/b)
+
+#hours to minute
+#x=int(input('enter no of hours to convert them into minutes: '))
+#print(x*60)
+
+#minutes to hours
+#x=int(input('enter no of minutes to convert them into hours:  '))
+#print(x/60)
+#dolars to inr
+#x=int(input('enter $: '))
+#print(x*48)
+
+#rs to dollars
+#x=int(input('enter rupees: '))
+#print(x/48)
+
+#dollars into pound
+#x=int(input('enter $: '))
+#print(x*48/70)
+
+#grams into kilograms
+x=int(input('enter gms: '))
+print(x/1000)
+
+
+
+
